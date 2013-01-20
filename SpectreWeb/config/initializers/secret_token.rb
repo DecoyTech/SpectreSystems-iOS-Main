c46c564f5271cb6a24d2bac38e4b4db90a4a228b
Brian <kikbal@decoytech.com>
2013-01-20 11:37:00 -0600

    added web rails skelly

---
+++ SpectreWeb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpectreWeb::Application.config.secret_token = '435ab445f86b412a3694dcfe3901f94a193c2cdaa418ff44d469296d2a24808af5bac451a748a0f1091db1e5fe43cdbefe414d0e1e472c155d37dcdb29e994cd'
